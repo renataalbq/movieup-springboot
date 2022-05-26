5e265e4b7cb99b21b095cb2c241941cea43b92ea
Renata Albuquerque <renataalbq1@gmail.com>
2022-05-26 17:36:29 -0300

    implementando repository filme e serie

--- src/main/java/br/edu/ifpb/movieup/repositories/FilmeRepository.java
+++ src/main/java/br/edu/ifpb/movieup/repositories/FilmeRepository.java
@@ -2,6 +2,22 @@ package br.edu.ifpb.movieup.repositories;
 
 import br.edu.ifpb.movieup.model.Filme;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface FilmeRepository  extends JpaRepository<Filme, Long> {
+    @Query(value = "SELECT f from Filme f where f.emalta='1' ")
+    List<Filme> buscarEmAlta(String opcao);
+
+    @Query(value = "SELECT f from Filme f where f.favoritos='1' ")
+    List<Filme> buscarFavoritos(String opcao);
+
+    @Query(value = "SELECT f from Filme f where f.aclamados='1' ")
+    List<Filme> buscarAclamados(String opcao);
+
+    @Query(value = "SELECT f from Filme f where f.esperados='1' ")
+    List<Filme> buscarEsperados(String opcao);
+    @Query(value = "SELECT f from Filme f where f.favsemana='1' ")
+    List<Filme> buscarFavSemana(String opcao);
 }
--- src/main/java/br/edu/ifpb/movieup/repositories/SerieRepository.java
+++ src/main/java/br/edu/ifpb/movieup/repositories/SerieRepository.java
@@ -1,7 +1,20 @@
 package br.edu.ifpb.movieup.repositories;
 
+import br.edu.ifpb.movieup.model.Filme;
 import br.edu.ifpb.movieup.model.Serie;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface SerieRepository extends JpaRepository<Serie, Long> {
+    @Query(value = "SELECT s from Serie s where s.emalta='1' ")
+    List<Serie> buscarEmAlta(String opcao);
+
+    @Query(value = "SELECT s from Serie s where s.vencedoras='1' ")
+    List<Serie> buscarVencedoras(String opcao);
+
+    @Query(value = "SELECT s from Serie s where s.lancamentos='1' ")
+    List<Serie> buscarLancamentos(String opcao);
+
 }
