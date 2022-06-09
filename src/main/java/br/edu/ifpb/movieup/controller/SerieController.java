c64441cdbf1c33c70a5672047cafeba851c05b87
Renata Albuquerque <renataalbq1@gmail.com>
2022-06-09 09:11:52 -0300

    alterações gerais

--- src/main/java/br/edu/ifpb/movieup/controller/SerieController.java
+++ src/main/java/br/edu/ifpb/movieup/controller/SerieController.java
@@ -8,32 +8,35 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-
+@RestController
+@RequestMapping("/series")
 public class SerieController {
 
     @Autowired
     private SerieService serieService;
 
 
-    @GetMapping("/series")
+    @GetMapping
     public List<Serie> getSeries() {
         return this.serieService.getSeries();
     }
 
-    @GetMapping("/series/emalta")
+    @GetMapping("/emalta")
     public ResponseEntity<List<Serie>> buscarEmAlta(String opcao){
         List <Serie> serie = serieService.buscarEmAlta(opcao);
         return new ResponseEntity<List<Serie>>(serie, HttpStatus.OK);
     }
 
-    @GetMapping("/series/vencedoras")
+    @GetMapping("/vencedoras")
     public ResponseEntity<List<Serie>> buscarVencedoras(String opcao){
         List <Serie> serie = serieService.buscarVencedoras(opcao);
         return new ResponseEntity<List<Serie>>(serie, HttpStatus.OK);
     }
-    @GetMapping("/series/lancamentos")
+    @GetMapping("/lancamentos")
     public ResponseEntity<List<Serie>> buscarLancamentos(String opcao){
         List <Serie> serie = serieService.buscarLancamentos(opcao);
         return new ResponseEntity<List<Serie>>(serie, HttpStatus.OK);
